d8d4f22bbac707da3a0bf7d5d7f62582d81c25ec
Nothing <maelapouge@gmail.com>
2026-05-09 14:06:29 +0200

    added node to launch and corrected python script

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pkg update && pkg upgrade
-pkg install python npm ffmpeg
+pkg install python npm ffmpeg nodejs
 python3 -m venv venv && source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirement.txt
